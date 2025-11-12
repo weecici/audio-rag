9b0274d774d942fbd358eeac4f6bf7219e974c16
CiCi <96775840+weecici@users.noreply.github.com.>
2025-11-12 12:41:47 +0700

    chore: update handful scripts

--- scripts/inngest.sh
+++ scripts/inngest.sh
@@ -1,1 +1,7 @@
-pnpx inngest-cli@latest dev -u http://localhost:8000/api/inngest --no-discovery --no-poll
+#!/usr/bin/env bash
+
+set -a  
+source "$(dirname "$0")/../.env"
+set +a
+
+pnpx inngest-cli@latest dev -u http://${BACKEND_HOST:-localhost}:${BACKEND_PORT:-8000}/api/inngest --no-discovery --no-poll
