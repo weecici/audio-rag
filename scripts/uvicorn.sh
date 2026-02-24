19fec459389f92ea1ca86662faf73d209819f41f
CiCi <96775840+weecici@users.noreply.github.com.>
2026-02-24 23:35:42 +0700

    fix(scripts): remove unused script and fix the uvicorn script

--- scripts/uvicorn.sh
+++ scripts/uvicorn.sh
@@ -4,4 +4,4 @@ set -a
 source "$(dirname "$0")/../.env"
 set +a
 
-uv run uvicorn src.main:app --host ${BACKEND_HOST:-localhost} --port ${BACKEND_PORT:-8000}
+uv run uvicorn app.main:app --host ${BACKEND_HOST:-localhost} --port ${BACKEND_PORT:-8000}
